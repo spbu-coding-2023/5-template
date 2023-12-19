5c2cfffa32cb019bf1f40f95b7c58779de96fd9a
Ilhom Kombaev <kombaev02@gmail.com>
2023-12-19 23:24:05 +0300

    Fix test_infix.sh (#2)

--- scripts/test_infix.sh
+++ scripts/test_infix.sh
@@ -2,14 +2,14 @@
 
 BASEDIR=$(realpath "$(dirname "$0")")
 ROOTDIR=$(realpath "$BASEDIR/..")
-TESTDIR=$(realpath "$ROOTDIR/test/basic")
+TESTDIR=$(realpath "$ROOTDIR/test/infix")
 CALCULATOR=$(realpath "$ROOTDIR/build/src/calculator")
 CHECKER="$BASEDIR/checker.py"
 CORRECT="correct"
 INCORRECT="incorrect"
 
-echo "\033[96mTest basic incorrect tests\033[0m"
+echo "\033[96mTest infix incorrect tests\033[0m"
 python3 "$CHECKER" "$CALCULATOR" "${TESTDIR}/${INCORRECT}"
 
-echo "\033[96mTest basic correct tests\033[0m"
+echo "\033[96mTest infix correct tests\033[0m"
 python3 "$CHECKER" "$CALCULATOR" "${TESTDIR}/${CORRECT}"
